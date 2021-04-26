1a30d274675f1fb06cadc279cdfc2d41d43f3f0e
Antony <antony.marques@hotmail.ch>
2021-04-26 13:17:48 +0200

    Add a form around the drop to see the impact

--- ressources/js/drop.js
+++ ressources/js/drop.js
@@ -21,14 +21,23 @@ class Drop
         this.dropImage.addEventListener('load', (event) => {
             this.width = this.dropImage.width; //Reduce width of the drop
             this.height = (this.dropImage.height/this.dropImage.width)*this.width; //Reduce height of the drop by keeping same ratio
-            this.dropReady = true
+            this.dropReady = true; //The image has been load, we can draw it
         });
 
         this.dropImage.src = "/ressources/images/game/DropSize" + this.size + ".png";
 
+        //Just to see for impact
+        this.context.beginPath();
+        this.context.moveTo(this.x-8, this.y+this.height);
+        this.context.lineTo(this.x+this.width+8, this.y+this.height);
+        this.context.lineTo(this.x+this.width/2, this.y-10);
+        this.context.fill();
+
         if(this.dropReady){
             this.context.drawImage(this.dropImage, this.x, this.y, this.width, this.height);
         }
+
+        
     }
 
     update(secondsPassed){
