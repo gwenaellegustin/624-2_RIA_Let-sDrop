f1641a221a45b42e44929244d6bb6a63ab1e4131
Antony <antony.marques@hotmail.ch>
2021-05-14 15:10:20 +0200

    find country

--- ressources/js/geolocalisation.js
+++ ressources/js/geolocalisation.js
@@ -1,23 +1,19 @@
 navigator.geolocation.getCurrentPosition(onSuccess);
 
-let lat, lng;
+let lat, lng, url;
 
 function onSuccess(position){
     lat = position.coords.latitude;
     lng = position.coords.longitude;
 
-    let mymap = L.map('geolocalisation').setView([lat, lng], 20);
+	url = 'https://nominatim.openstreetmap.org/reverse?lat='+lat+'&lon='+lng;
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(mymap);
+	console.log(lat+","+lng);
+
+	userAction();
 }
 
-
-
-
+const userAction = async () => {
+	const response = await fetch('https://nominatim.openstreetmap.org/reverse?lat='+lat+'&lon='+lng+'&format=json');
+	const myJson = await response.json();
+}
