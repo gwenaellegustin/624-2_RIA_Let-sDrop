e7978c7d8d46729ccf00d686a36a2e7501fa9557
Antony <antony.marques@hotmail.ch>
2021-05-29 15:07:03 +0200

    first edit

--- ressources/js/Level0/geolocation.js
+++ ressources/js/Level0/geolocation.js
@@ -1,38 +1,36 @@
 class Geolocation {
-	static draw(){
-		let options = {
+	static onSuccess(position){		
+		getDataFromLatLong(position.coords.latitude, position.coords.longitude);
+	}
+
+	static onError() {
+		document.getElementById('geolocation_region').innerHTML = '<br/>' + 'Escape from your reality';
+	}
+
+	static getOptions(){
+		return options = {
 			enableHighAccuracy: false,
 			timeout: 5000,
 			maximumAge: 0
 		}
+	}
 
-		navigator.geolocation.watchPosition(onSuccess, onError, options);
+	static async getDataFromLatLong(lat, lng){
+		const response = await fetch('https://nominatim.openstreetmap.org/reverse?lat='+lat+'&lon='+lng+'&format=json');
+		const result = await response.json();
 
-		function onSuccess(position){		
-			getDataFromLatLong(position.coords.latitude, position.coords.longitude);
+		let town = result['address']['village'];
+		let state = result['address']['state'];
+		let country = result['address']['country'];
+
+		if(!state){
+			document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + country;
 		}
-
-		function onError() {
-			document.getElementById('geolocation_region').innerHTML = '<br/>' + 'Escape from your reality';
+		else if(!town){
+			document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + '<br/>' + state + ', ' + country;
 		}
-
-		const getDataFromLatLong = async (lat, lng) => {
-			const response = await fetch('https://nominatim.openstreetmap.org/reverse?lat='+lat+'&lon='+lng+'&format=json');
-			const result = await response.json();
-	
-			let town = result['address']['village'];
-			let state = result['address']['state'];
-			let country = result['address']['country'];
-
-			if(!state){
-				document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + country;
-			}
-			else if(!town){
-				document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + '<br/>' + state + ', ' + country;
-			}
-			else{
-				document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + '<br/>' + town + ' (' + state + ', ' + country + ')';
-			}
+		else{
+			document.getElementById('geolocation_region').innerHTML = 'Escape from: ' + '<br/>' + town + ' (' + state + ', ' + country + ')';
 		}
-	}	
+	}
 }
